2e3a7ebc65a4d16b9e39beed5556dc4685edbbc3
Rockwell Allen <rockwellallen@gmail.com>
2022-02-05 12:20:15 -0800

    routers and comtroller completed

--- server/controller.js
+++ server/controller.js
@@ -4,8 +4,8 @@ const controller = {
   students: {
     getStudents: function (req, res) {
       // TODO: add your code here to fetch all students
-      queryStr = 'SELECT * FROM students';
-      queryArgs = [];
+      var queryStr = 'SELECT * FROM students JOIN images ON students.id = images.id';
+      var queryArgs = [];
 
       db.query(queryStr, queryArgs, (err, results) => {
         if (err) {
@@ -17,17 +17,44 @@ const controller = {
     },
     postName: function (req, res) {
       // TODO: add your code here to add a student's name
-
+      var queryStr = 'INSERT INTO students (name) VALUES (?)';
+      var queryArgs = [req.body.name];   // make sure this is the right req path
+      // console.log(queryArgs);
+      db.query(queryStr, queryArgs, (err, results) => {
+        if (err) {
+          res.status(404).send(err);
+        } else {
+          res.status(201).send(results);
+        }
+      })
     },
     updateName: function (req, res) {
       // TODO: add your code here to update a student's name
+      var queryStr = 'UPDATE students SET name=(?) WHERE id=(?)';
+      var queryArgs = [req.body.name, req.params.id];   // make sure this is the right req path
 
+      db.query(queryStr, queryArgs, (err, results) => {
+        if (err) {
+          res.status(404).send(err);
+        } else {
+          res.status(202).send(results);
+        }
+      });
     }
   },
   images: {
     postImg: function (req, res) {
       // TODO: add your code here to add a student image
+      var queryStr = 'INSERT INTO images (imgurl) VALUES (?)';
+      var queryArgs = [req.body.image];  // make sure this is the right req path
 
+      db.query(queryStr, queryArgs, (err, results) => {
+        if (err) {
+          res.status(404).send(err);
+        } else {
+          res.status(203).send(results);
+        }
+      })
     }
   }
 };
